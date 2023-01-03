9dea2b338e43847161f2118b5ef0489320332269
Ore Akinwole <oreakinwole@gmail.com>
2023-01-02 21:08:53 -0500

    create user controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import config from './config/keys';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(config.mongoURI)],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://taydrey:damid509@cluster0.ha3ke.mongodb.net/?retryWrites=true&w=majority',
+};
